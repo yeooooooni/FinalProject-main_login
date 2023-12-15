7bb37c8a91327c3aac7ea634463a0babd7c0a8c3
yeon <22100141@handong.ac.kr>
2023-12-16 02:15:57 +0900

    initial commit

--- src/main/java/com/crud/example/BoardController.java
+++ src/main/java/com/crud/example/BoardController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-//please
+
 @Controller
 @RequestMapping(value="/board")
 public class BoardController {
